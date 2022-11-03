51cab94e0cea9bd9b5026c1d5fe2db8ad44703de
Edigleis Pereira <edigleisbuiu@hotmail.com>
2022-11-03 17:52:23 -0400

    🚧: Em desenvolvimento

--- src/js/lista-tarefa.js
+++ src/js/lista-tarefa.js
@@ -1,2 +1,12 @@
 import './nav.js'
 import '../css/lista-tarefa.css'
+
+const taref = document.querySelector('#input-nova-tarefa')
+const adiciona = document.querySelector('#btn-adicionar')
+const listaTarefa = document.querySelector("Lista-tarefas")
+
+adiciona.addEventListener('click', function(event){
+    event.target.innerHTML = 'Adicionado'
+});
+document.createElement('clicar')
+ 
