896bfc93a6c707dfef109d68cf30185e6e1e7d52
Edigleis Pereira <edigleisbuiu@hotmail.com>
2022-11-03 21:43:27 -0400

    ✨ Adicionar Tarefa

--- src/js/lista-tarefa.js
+++ src/js/lista-tarefa.js
@@ -3,10 +3,21 @@ import '../css/lista-tarefa.css'
 
 const taref = document.querySelector('#input-nova-tarefa')
 const adiciona = document.querySelector('#btn-adicionar')
-const listaTarefa = document.querySelector("Lista-tarefas")
+const listaTarefa = document.querySelector("#lista-tarefas")
 
-adiciona.addEventListener('click', function(event){
-    event.target.innerHTML = 'Adicionado'
+adiciona.addEventListener('click', function(){
+    let descricaoTarefa = taref.value
+    const li = document.createElement('li')
+    li.className = 'tarefa'
+    li.innerHTML = `
+	<div>${descricaoTarefa}</div>
+    <button class="btn-excluir">Excluir</button>
+    `
+    listaTarefa.appendChild(li)
+    console.log("Tarefa Adicionada")
+
+    li.addEventListener('click', function(event){
+        
+    })
 });
-document.createElement('clicar')
  
